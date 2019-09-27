2e60988ec7655a45f1784c0d31ea64c66d6d88a3
sximada <50688746+sximada@users.noreply.github.com>
2019-09-27 19:33:41 +0900

    Add keycloak settings in environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  KEYCLOAK_URL: 'https://localhost:8443/auth',
+  KEYCLOAK_REALM: 'demo',
+  KEYCLOAK_CLIENTID: 'kc-angular',
 };
 
 /*
